53f7021bcb15106193554ffd00c23e65e497812e
Pedro Pimenta <pedrogpimenta@users.noreply.github.com>
2019-09-18 16:57:48 +0100

    Change keys to press for volume + Remove backlight

--- backlight_hotkey.py
+++ backlight_hotkey.py
@@ -1,7 +1,6 @@
 import time
 from evdev import InputDevice
 from select import select
-from tools.brightness_tool import BrightnessTool
 import alsaaudio
 
 m = alsaaudio.Mixer()
@@ -10,26 +9,11 @@ sound_volume=m.getvolume()[0]
 
 dev = InputDevice('/dev/input/event1')
 
-select_press=False
 while True:
     select([dev], [], [])
     for event in dev.read():
         # print "code:%s value:%s" % (event.code, event.value)
-        if (event.code==57 and event.value==1):
-            select_press=True
-            # print("sel press")
-        elif(event.code==57 and event.value==0):
-            select_press=False
-        
-        if(select_press==True and event.code==103 and event.value==1):
-            brightnessTool = BrightnessTool()
-            brightnessTool.StepBack()
-            del brightnessTool
-        elif(select_press==True and event.code==108 and event.value==1):
-            brightnessTool = BrightnessTool()
-            brightnessTool.Further()
-            del brightnessTool
-        elif(select_press==True and event.code==105 and event.value==1):
+        elif(event.code==74 and event.value==1):
             sound_volume=sound_volume - 20
             if(sound_volume<0):
                 sound_volume=0
@@ -38,18 +22,12 @@ while True:
                 m.setvolume(sound_volume)
             except Exception,e:
                 print(str(e))
-        elif(select_press==True and event.code==106 and event.value==1):
+        elif(event.code==78 and event.value==1):
             sound_volume=sound_volume + 20
             if(sound_volume>100):
                 sound_volume=98
-
             try:
                 m = alsaaudio.Mixer()
                 m.setvolume(sound_volume)
             except Exception,e:
                 print(str(e))
-
-        
-            
-
-
